6310e4aefa0de92a96ac5fd7ff96ab5365b05b8d
Gourav Mishra <88934396+gouravmarch20@users.noreply.github.com>
2021-09-23 13:21:47 +0530

    refactor css

--- js/app.js
+++ js/app.js
@@ -2,17 +2,21 @@ const textInput = document.querySelector('#text-input')
 const btnTranslate = document.querySelector('#btn-translate')
 const outputDiv = document.querySelector('#output-div')
 
-const url = 'https://api.funtranslations.com/translate/yoda.json?'
+const url = 'https://api.funtranslations.com/translate/yoda.json'
 
 function translateText () {
   const userInput = textInput.value
-  fetch(`url${userInput}`)
-    .then(response => response.json())
-    .then(data => {
-      console.log('Success:', data)
-    })
-    .catch(error => {
-      console.error('Error:', error)
-    })
+  console.log(`${url}?text=${userInput}`)
+
+//   fetch(`${url}?text=${userInput}`)
+//     .then(response => response.json())
+//     .then(data => {
+     outputDiv.innerText = "asfd"
+     outputDiv.style.backgroundColor = "#1C8D73"
+
+    // })
+    // .catch(error => {
+    //   console.error('Error:', error)
+    // })
 }
 btnTranslate.addEventListener('click', translateText)
